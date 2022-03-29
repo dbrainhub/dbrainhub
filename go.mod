69ab158a469682b1e11d4953cd67c54bc3b6b8f5
lypxhxjj <792512795@qq.com>
2022-03-29 16:31:51 +0800

    规范化handler格式

--- go.mod
+++ go.mod
@@ -3,12 +3,10 @@ module github.com/dbrainhub/dbrainhub
 go 1.16
 
 require (
-	github.com/BurntSushi/toml v1.0.0 // indirect
+	github.com/BurntSushi/toml v1.0.0
 	github.com/gin-gonic/gin v1.7.2-0.20220325004437-205bb8151cb7
-	github.com/go-kratos/kratos/v2 v2.2.1
 	github.com/natefinch/lumberjack v2.0.0+incompatible
 	go.uber.org/zap v1.21.0
-	google.golang.org/genproto v0.0.0-20220324131243-acbaeb5b85eb
 	google.golang.org/protobuf v1.28.0
 	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
 
--- handler/api_resp.go
+++ handler/api_resp.go
@@ -2,74 +2,22 @@ package handler
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 
 	"github.com/dbrainhub/dbrainhub/errors"
 	"github.com/gin-gonic/gin"
 )
 
-const (
-	RetSuccess = 1
-	RetFailure = -1
-)
-
-func BadRequestResp(c *gin.Context, msg string) {
-	resp := &ApiError{
+func BadRequestError(format string, a ...interface{}) error {
+	return &ApiError{
 		Code: http.StatusBadRequest,
 		Name: "BadRequestError",
-		Msg:  msg,
+		Msg:  fmt.Sprintf(format, a...),
 	}
-	resp.RenderJson(c)
 }
 
-func FailResp(c *gin.Context, err error) {
-	resp := newApiError(err)
-	resp.RenderJson(c)
-}
-
-func SuccessResp(c *gin.Context, data interface{}) {
-	resp := &ApiSucc{
-		Ret:  RetSuccess,
-		Data: data,
-	}
-	resp.RenderJson(c)
-}
-
-type (
-	ApiSucc struct {
-		Ret  int32       `json:"ret"`
-		Data interface{} `json:"data"`
-	}
-
-	ApiError struct {
-		Ret  int32  `json:"ret"`
-		Code int32  `json:"code"`
-		Name string `json:"name"`
-		Msg  string `json:"msg"`
-	}
-)
-
-func (a *ApiSucc) RenderJson(g *gin.Context) {
-	statusCode := http.StatusOK
-	g.JSON(statusCode, a)
-}
-
-func (a *ApiError) Error() string {
-	res, _ := json.Marshal(a)
-	return string(res)
-}
-
-func (a *ApiError) RenderJson(c *gin.Context) {
-	statusCode := http.StatusOK
-	if a.Code == http.StatusInternalServerError || a.Code == http.StatusBadRequest {
-		statusCode = int(a.Code)
-	}
-
-	a.Ret = RetFailure
-	c.AbortWithStatusJSON(statusCode, a)
-}
-
-func newApiError(err error) *ApiError {
+func NewApiError(err error) *ApiError {
 	switch err.(type) {
 	case *ApiError:
 		return err.(*ApiError)
@@ -87,4 +35,25 @@ func newApiError(err error) *ApiError {
 			Msg:  err.Error(),
 		}
 	}
+}
+
+type ApiError struct {
+	Code int32  `json:"code"`
+	Name string `json:"name"`
+	Msg  string `json:"msg"`
+}
+
+func (a *ApiError) Error() string {
+	res, _ := json.Marshal(a)
+	return string(res)
+}
+
+func (a *ApiError) RenderJson(c *gin.Context) {
+	statusCode := http.StatusBadRequest
+
+	if a.Code < errors.MinCustomErrorCode {
+		statusCode = int(a.Code)
+	}
+
+	c.AbortWithStatusJSON(statusCode, a)
 }
