673b347475f7bfe24940e665267e398f10e65dfe
lypxhxjj <792512795@qq.com>
2022-04-19 17:20:33 +0800

    add os_version and kernal_version

---
+++ utils/os/os_version_test.go
@@ -0,0 +1,21 @@
+package osutils
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetOsVersion(t *testing.T) {
+	var drawin darwinVersionQuerier
+	version, err := drawin.GetOsVersion()
+	assert.Nil(t, err)
+	assert.Equal(t, version, "10.15.7")
+}
+
+func TestGetKernalVersion(t *testing.T) {
+	var drawin darwinVersionQuerier
+	version, err := drawin.GetKernalVersion()
+	assert.Nil(t, err)
+	assert.Equal(t, version, "19.6.0")
+}
