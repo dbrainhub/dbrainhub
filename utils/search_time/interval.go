b0610075e080fa3ed12fc28901f36a4c57afea87
aaronkan007 <aaronkan007@gmail.com>
2022-04-27 16:30:33 +0800

    Refactor time interval func (#27)
    * (feat): Add dbrainhub output
    
    * (feat) Add rate limiter for dbrainhub output
    
    * go.mod
    
    * move rate_limit to utils
    
    * format output config
    
    * Refactor dbrainhub output as the filebeat es output format
    
    * remove unused error
    
    * Add es search time interval func
    
    * (feat): Add search api for db instance
    
    * add rpc title
    
    * add rpc title
    
    * refactor time interval func
    
    * refactor time interval func
    
    * fmt
    
    Co-authored-by: songkan16551 <songkan16551@ipalfish.com>

--- utils/search_time/interval.go
+++ utils/search_time/interval.go
@@ -2,49 +2,47 @@ package search_time
 
 import "time"
 
-// refer: https://github.com/elastic/kibana/blob/main/src/plugins/vis_types/timelion/common/lib/calculate_interval.ts#L13
+var Intervals = []Interval{
+	{500, "100ms"},
+	{5000, "1s"},
+	{7500, "5s"},
+	{15000, "10s"},
+	{45000, "30s"},
+	{180000, "1m"},
+	{450000, "5m"},
+	{1200000, "10m"},
+	{2700000, "30m"},
+	{7200000, "1h"},
+	{21600000, "3h"},
+	{86400000, "12h"},
+	{604800000, "24h"},
+	{1814400000, "1w"},
+	{3628800000, "30d"},
+}
+
+type Interval struct {
+	IntervalSecond float64
+	IntervalString string
+}
+
 func GetInterval(from, to string) string {
+	intervalStr := "1y"
 	fromT, err := time.ParseInLocation("2006-01-02T15:04:05Z", from, time.Local)
 	if err != nil {
-		return "1y"
+		return intervalStr
 	}
 	toT, err := time.ParseInLocation("2006-01-02T15:04:05Z", to, time.Local)
 	if err != nil {
-		return "1y"
+		return intervalStr
 	}
 	interval := toT.Sub(fromT).Seconds()
-	switch true {
-	case interval <= 500: // <= 0.5s
-		return "100ms"
-	case interval <= 5000: // <= 5s
-		return "1s"
-	case interval <= 7500: // <= 7.5s
-		return "5s"
-	case interval <= 15000: // <= 15s
-		return "10s"
-	case interval <= 45000: // <= 45s
-		return "30s"
-	case interval <= 180000: // <= 3m
-		return "1m"
-	case interval <= 450000: // <= 9m
-		return "5m"
-	case interval <= 1200000: // <= 20m
-		return "10m"
-	case interval <= 2700000: // <= 45m
-		return "30m"
-	case interval <= 7200000: // <= 2h
-		return "1h"
-	case interval <= 21600000: // <= 6h
-		return "3h"
-	case interval <= 86400000: // <= 24h
-		return "12h"
-	case interval <= 604800000: // <= 1w
-		return "24h"
-	case interval <= 1814400000: // <= 3w
-		return "1w"
-	case interval < 3628800000: // <  2y
-		return "30d"
-	default:
-		return "1y"
+
+	for _, v := range Intervals {
+		if interval <= v.IntervalSecond {
+			intervalStr = v.IntervalString
+			break
+		}
 	}
+
+	return intervalStr
 }
